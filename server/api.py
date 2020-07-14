382a20b720cbc557ce6c4aff67405da32a36b4a5
bismarckserve <bismarck@pythonserve.org>
2020-07-14 19:04:21 +0000

    Complete Server Implementation

--- server/api.py
+++ server/api.py
@@ -6,45 +6,72 @@ app = Flask(__name__)
 api = Api(app)
 
 class Establish(Resource):
-	client1IP = 0
-	client2IP = 0
 	def get(self):
-		if (client1IP == 0):
-			client1IP = request.remote_addr
-		
-		elif (client2IP):
-			client2IP = request.remote_addr
+		with open("sessionData.json", "r+") as sessData:
+			session = json.load(sessData)
 
-		else:
-			return "Server session is full!"
+			if (session["client1IP"] == " "):
+				session["client1IP"] = request.remote_addr
+				sessData.seek(0)
+				json.dump(session, sessData)
+				sessData.truncate()
+
+
+			elif (session["client2IP"] == " "):
+				session["client2IP"] = request.remote_addr
+				sessData.seek(0)
+				json.dump(session, sessData)
+				sessData.truncate()
+
+			else:
+				return "Server session is full!"
 
 class Data(Resource):
 	def get(self):
-		if (request.remote_addr == Establish.client1IP):
-			dataFile = open("client1.json", "r")
-			data = json.load(dataFile)
-			return data
 
-		if (request.remote_addr == Establish.client2IP):
-			dataFile = open("client2.json", "r")
-			data = json.load(dataFile)
-			return data
+		with open("sessionData.json", "r") as sessData:
+			session = json.load(sessData)
+
+			if (request.remote_addr == session["client1IP"]):
+				with open("client1.json", "r") as dataFile:
+					data = json.load(dataFile)
+	
+					return data, 201
+
+			elif (request.remote_addr == ["client2IP"]):
+				with open("client2.json", "r") as dataFile:
+					data = json.load(dataFile)
+					return data, 201
+		
+			else:
+				return 'Unhandled IP:' + session["client1IP"] + '!=' + request.remote_addr + '.\nUndefined game session\n', 300
 
 	def post(self):
-		dataIn = request.get_json()
 
-		if (request.remote_addr == Establish.client1IP):
-			dataFile = open("client1.json", "w")
-			json.dump(dataIn, dataFile)
+		with open("sessionData.json", "r") as sessData:
+			session = json.load(sessData)
 
-		if (request.remote_addr == Establish.client2IP):
-			dataFile = open("client2.json", "w")
-			json.dump(dataIn, dataFile)
+			dataIn = request.get_json()
 
-		return 201
+			if (request.remote_addr == session["client1IP"]):
+				with open("client1.json", "w") as dataFile:
+					json.dump(dataIn, dataFile)
+	
+				return 202
+
+			elif (request.remote_addr == Establish.client2IP):
+				with open("client2.json", "w") as dataFile:
+					json.dump(dataIn, dataFile)
+	
+					return 202
+		
+			else:
+				return 'Unhandled IP: undefined game session', 300
+
 
 api.add_resource(Establish, '/')
 api.add_resource(Data, '/session')
 
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host='0.0.0.0')
