05d134c0dc56587a6ba20de05469b855dda82e5b
oxmawagyu <octaviomvguimaraes@gmail.com>
2021-07-24 12:07:27 +0200

    UI Colors Test: Dark Theme + New Font

--- test/ui_colors/main.cpp
+++ test/ui_colors/main.cpp
@@ -116,16 +116,16 @@ int main(int arc, char* argv[]) {
 
     // COLORS ###################################
     // LIGHT THEME
-        SDL_Colour st_bckgr_clr {255, 255, 204, 255};
-        SDL_Colour st_mn_clr {179, 224, 255, 50};
-        SDL_Colour st_mn_btt_clr {0, 122, 204, 255};
-        SDL_Colour st_mn_ft_clr {230, 255, 230, 255};
+        SDL_Colour st_bckgr_clr {13, 13, 13, 255};
+        SDL_Colour st_mn_clr {166, 166, 166, 50};
+        SDL_Colour st_mn_btt_clr {77, 0, 0, 255};
+        SDL_Colour st_mn_ft_clr {242, 242, 242, 255};
     
     // DARK THEME
-        // SDL_Colour st_bckgr_clr {54, 54, 54, 255};
-        // SDL_Colour st_mn_clr {26, 26, 26, 50};
-        // SDL_Colour st_mn_btt_clr {255, 51, 51, 255};
-        // SDL_Colour st_mn_ft_clr {0, 0, 0, 255};
+        // SDL_Colour st_bckgr_clr {13, 13, 13, 255};
+        // SDL_Colour st_mn_clr {166, 166, 166, 50};
+        // SDL_Colour st_mn_btt_clr {77, 0, 0, 255};
+        // SDL_Colour st_mn_ft_clr {242, 242, 242, 255};
     // ###########################################
 
     std::cout << "Test printf 1...\n";
